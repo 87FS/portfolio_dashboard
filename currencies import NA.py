36619afe4056955db6474e921beeb1df5edc5b1c
FS87 <filip.szeszula87@gmail.com>
2021-06-28 12:52:18 +0200

    cleared libraries

--- currencies import NA.py
+++ currencies import NA.py
@@ -3,7 +3,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import numpy as np
 import yfinance as yf
-import pandas_datareader.data as web
 import time
 
 json_cred = r'C:\Users\nauka\Desktop\projekt portfolio\credentials.json'
