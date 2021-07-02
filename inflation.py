2c9777f962b5809114eb5fb5160497b5a1aac252
FS87 <filip.szeszula87@gmail.com>
2021-07-02 15:09:02 +0200

    working early solution

--- inflation.py
+++ inflation.py
@@ -28,7 +28,44 @@ formatted.sort_values(by = ["Year", "Month"], inplace = True, ignore_index = Tru
 ## here is the issue whether to count current month inflation or not, for now I decided not to
 start_date = "2019-04-08" ## placeholder, it will be the min of all dates in purchases spreadsheet
 cpi = formatted[formatted["Date"] >= start_date][["Date", "CPIm2m"]].copy(deep = True)
+cpi["CPIm2m"] = cpi["CPIm2m"]/100
 cpi.reset_index(drop = True, inplace=True)
 cpi.dropna(inplace = True)
 
+stock = pd.read_csv(
+                    "fb.csv"
+                    , delimiter = ","
+                    , header = 0
+                    )
 
+stock["Date"] = stock["Date"].astype(np.datetime64)
+
+merged = pd.merge(
+                    left = stock
+                    , right = cpi
+                    , on = "Date"
+                    , how = "inner"
+                    )
+merged.drop(columns = "Average Price in PLN", inplace = True)
+
+merged["Total Purchase in PLN CPI adj"] = np.nan
+
+initial_purchase = merged.loc[0,["Total Purchase in PLN"]][0]
+first_cpi = merged.loc[0,["CPIm2m"]][0]
+
+merged.loc[0,["Total Purchase in PLN CPI adj"]] = initial_purchase * first_cpi
+
+for index, row in merged[1:].iterrows():
+    merged.loc[index, ["Total Purchase in PLN CPI adj"]] = merged.loc[index-1, ["Total Purchase in PLN CPI adj"]] * row["CPIm2m"]
+
+
+end = pd.merge(
+                left = stock
+                , right = merged[["Date", "Total Purchase in PLN CPI adj"]]
+                , on = "Date"
+                , how = "left"
+                )
+
+end.loc[0, ["Total Purchase in PLN CPI adj"]] = initial_purchase
+
+end["Total Purchase in PLN CPI adj"].fillna(method = 'ffill', inplace = True)
