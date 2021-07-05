b9bfc192fbaac3d758abd6ef7260eee6d2aa2723
FS87 <filip.szeszula87@gmail.com>
2021-07-05 09:32:42 +0200

    fixed formatting

--- fullimport.py
+++ fullimport.py
@@ -8,7 +8,7 @@ import time
 
 json_cred = r'C:\Users\nauka\Desktop\projekt portfolio\credentials.json'
 gsheet = "portfolio"
-wksheet = "transactions"
+wksheet = "transactions2"
 
 def gspread_parser(json_cred = json_cred, spreadsheet = gsheet, worksheet = wksheet):
     ''' fetches the google spreadsheet with the history of stock purchases
@@ -26,7 +26,10 @@ def gspread_parser(json_cred = json_cred, spreadsheet = gsheet, worksheet = wksh
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_cred, scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+                                                                    json_cred
+                                                                    , scope
+                                                                    )
     gc = gspread.authorize(credentials)
     wks = gc.open(spreadsheet).worksheet(worksheet)
 
@@ -34,7 +37,10 @@ def gspread_parser(json_cred = json_cred, spreadsheet = gsheet, worksheet = wksh
     data = wks.get_all_values()
     headers = data.pop(0)
 
-    purchases = pd.DataFrame(data, columns=headers)
+    purchases = pd.DataFrame(
+                            data
+                            , columns=headers
+                            )
 
     ## fixing comma separated decimals in g-sheets
     purchases["Purchase Price"] = purchases["Purchase Price"].str.replace(",", ".")
@@ -71,11 +77,13 @@ def currency_parser(investments):
     currency_prices_dataframes = []
     for pair in pairs:
         if pair != "PLN/PLN":
-            currency_raw = web.DataReader( pair
-                                      , "av-forex-daily"
-                                      , start = currency_start_date
-                                      , end = np.datetime64("today")
-                                      , api_key = "PLPX5PE8L7HVWLJ7")
+            currency_raw = web.DataReader(
+                                          pair
+                                          , "av-forex-daily"
+                                          , start = currency_start_date
+                                          , end = np.datetime64("today")
+                                          , api_key = "PLPX5PE8L7HVWLJ7"
+                                          )
             ## adding column with ticker
             currency_raw["Symbol"] = pair
             currency_raw["Currency"] = pair.replace("/PLN", "")
@@ -90,21 +98,26 @@ def currency_parser(investments):
             currency_raw = currency_raw[["Date", "Symbol", "Currency", "Exchange Rate to PLN"]]
 
                 ## filling missing data (no weekends, etc)
-            all_days = pd.date_range(start = currency_start_date
-                                , end = np.datetime64("today")
-                                , freq = "1d")
+            all_days = pd.date_range(
+                                    start = currency_start_date
+                                    , end = np.datetime64("today")
+                                    , freq = "1d"
+                                    )
 
-            df_all_days = pd.DataFrame(all_days, columns = ["Date"])
+            df_all_days = pd.DataFrame(
+                                        all_days
+                                        , columns = ["Date"]
+                                        )
 
-            currency_fixed = pd.merge(left = df_all_days
-                                     , right = currency_raw
-                                     , on = "Date"
-                                     , how = "left")
+            currency_fixed = pd.merge(
+                                        left = df_all_days
+                                        , right = currency_raw
+                                        , on = "Date"
+                                        , how = "left"
+                                        )
 
             ## filling missing data with last proper value
             currency_fixed.fillna(method = "ffill", inplace = True)     
-
-
         
             currency_prices_dataframes.append(currency_fixed)
 
@@ -132,12 +145,13 @@ purchases_history = gspread_parser()
 
 currencies_history = currency_parser(purchases_history)
 
-purchases_history = pd.merge( purchases_history
-                      , currencies_history
-                      , how = "inner"
-                      , left_on = ["Purchase Date", "Currency"]
-                      , right_on = ["Date", "Currency"]
-                      )
+purchases_history = pd.merge(
+                            purchases_history
+                            , currencies_history
+                            , how = "inner"
+                            , left_on = ["Purchase Date", "Currency"]
+                            , right_on = ["Date", "Currency"]
+                            )
 
 purchases_history.drop(columns = [ "Date", "Symbol" ], inplace = True)
 
@@ -162,14 +176,14 @@ def stock_parser(investments):
 
     link = main+raport+file
 
-    ## getting polish CPI data, the file is permanent and updated every month
+    ## the file is permanent and updated every month
     df = pd.read_excel(
                         link
                         , header = 0
                         , usecols = [2,3,4,5]
                         )
 
-    ## slicing just the month to month cpi comparison
+    ## slicing for just the month to month cpi comparison
     formatted = df[df["Sposób prezentacji"] == "Poprzedni miesiąc = 100"].copy(deep=True)
     formatted.drop(columns = ["Sposób prezentacji"], inplace = True)
     formatted.rename(columns = {"Rok":"Year", "Miesiąc":"Month", "Wartość":"CPIm2m"}, inplace = True)
@@ -204,16 +218,18 @@ def stock_parser(investments):
                         ## getting stocks data (date, price, dividends and splits)    
             stock = yf.Ticker(ticker["Ticker"])
 
-            stock_start_date = pd.to_datetime(ticker["Purchase Date"]
-                                              , dayfirst=False
-                                              , yearfirst = True
-                                              )
+            stock_start_date = pd.to_datetime(
+                                                ticker["Purchase Date"]
+                                                , dayfirst=False
+                                                , yearfirst = True
+                                                )
             ## downloading price history
-            prices_raw = stock.history(start = stock_start_date
-                                           , end = np.datetime64("today")
-                                           , interval = "1d"
-                                           , auto_adjust = False # True adjusts backwards for dividends
-                                           )
+            prices_raw = stock.history(
+                                        start = stock_start_date
+                                        , end = np.datetime64("today")
+                                        , interval = "1d"
+                                        , auto_adjust = False # True adjusts backwards for dividends
+                                        )
 
 
             ## adding ticker name
@@ -231,31 +247,32 @@ def stock_parser(investments):
 
             prices_raw_cleared.rename(columns = {"index" : "Date",
                                                  "Close" : "Price"}
-                                          , inplace = True
-                                          )
+                                      , inplace = True
+                                      )
             
             prices_raw_cleared["Date"] = prices_raw_cleared["Date"].astype(np.datetime64)
             
             ## filling missing data (no weekends, etc)
-            all_days = pd.date_range(start = stock_start_date
-                                     , end = np.datetime64("today")
-                                     , freq = "1d"
-                                     )
+            all_days = pd.date_range(
+                                    start = stock_start_date
+                                    , end = np.datetime64("today")
+                                    , freq = "1d"
+                                    )
 
-            df_all_days = pd.DataFrame(all_days
-                                       , columns = ["Date"]
-                                       )
-
-            prices_fixed = pd.merge(left = df_all_days
-                                        , right = prices_raw_cleared
-                                        , on = "Date"
-                                        , how = "left"
+            df_all_days = pd.DataFrame(
+                                        all_days
+                                        , columns = ["Date"]
                                         )
 
-            ## filling missing data with last proper value
-            prices_fixed.fillna(method = "ffill"
-                                    , inplace = True
+            prices_fixed = pd.merge(
+                                    left = df_all_days
+                                    , right = prices_raw_cleared
+                                    , on = "Date"
+                                    , how = "left"
                                     )
+
+            ## filling missing data with last proper value
+            prices_fixed.fillna(method = "ffill", inplace = True)
 
 
             stock_prices_dataframes.append(prices_fixed)
@@ -263,7 +280,9 @@ def stock_parser(investments):
 
                         ## CREATING PORTFOLIO HISTORY TABLE
             
-            stock_amounts = prices_fixed[ ["Date", "Ticker", "Stock Splits"]].copy(deep = True)
+            stock_amounts = prices_fixed[ ["Date"
+                                           , "Ticker"
+                                           , "Stock Splits"] ].copy(deep = True)
           
             ## checking if there were any splits
             if stock_amounts["Stock Splits"].sum() > 0:
@@ -271,21 +290,22 @@ def stock_parser(investments):
                 ## filling missing values in Stock Splits column to have a denominator for amounts
                 (stock_amounts["Stock Splits"]
                                         .replace(to_replace = 0.0
-                                                , method = "ffill"
-                                                , inplace = True))
+                                                 , method = "ffill"
+                                                 , inplace = True))
                 (stock_amounts["Stock Splits"]
                                         .replace(to_replace = 0.0
-                                                , value = 1
-                                                , inplace = True))
+                                                 , value = 1
+                                                 , inplace = True))
                 
                 ## adding new columns with proper amount of stocks (split adjusted)
                 stock_amounts["Value Amount"] = float(ticker["Purchase Amount"])
                 stock_amounts["Dividend Amount"] = float(ticker["Purchase Amount"])
                 
                 stock_amounts["Value Amount"] = (stock_amounts["Value Amount"]
-                                                             *
-                                                     stock_amounts.iloc[-1]["Stock Splits"] ## [-1] has the split value
+                                                             *      ## [-1] has the split value
+                                                     stock_amounts.iloc[-1]["Stock Splits"] 
                                                      )
+                
                 stock_amounts["Dividend Amount"] = (stock_amounts["Dividend Amount"]
                                                                 *
                                                         stock_amounts["Stock Splits"]
@@ -299,15 +319,17 @@ def stock_parser(investments):
                 
             ## adding total purchase in PLN to easily calculate combined average price
             stock_amounts["Total Purchase in PLN"] = (float(ticker["Purchase Price"])
-                                                      *
+                                                              *
                                                       float(ticker["Purchase Amount"])
-                                                      *
+                                                              *
                                                       float(ticker["Exchange Rate to PLN"]))
             
-            stock_amounts = pd.merge(left = df_all_days
-                                             , right = stock_amounts
-                                             , on = "Date"
-                                             , how = "left")
+            stock_amounts = pd.merge(
+                                    left = df_all_days
+                                    , right = stock_amounts
+                                    , on = "Date"
+                                    , how = "left"
+                                    )
             
             ## filling missing data with last proper value
             stock_amounts.fillna(method = "ffill", inplace = True)
@@ -349,14 +371,12 @@ def stock_parser(investments):
                                 , on = "Date"
                                 , how = "left"
                                 )
+            
             ## filling first value (the initial CPI 
             complete.loc[0, ["Total Purchase in PLN CPI adj"]] = initial_purchase
 
             ## filling the rest of the NaNs
             complete["Total Purchase in PLN CPI adj"].fillna(method = 'ffill', inplace = True)
-
-
-
 
             ## Date must become an index for the sake of .add() 
             complete.set_index("Date", inplace = True) 
@@ -386,10 +406,19 @@ def stock_parser(investments):
         ## combining all purchases of the same ticker into one dataframe        
             base = single_ticker_dataframes[0].copy(deep = True)
             for i in range(1, len(single_ticker_dataframes)):
-                base[["Total Purchase in PLN", "Value Amount", "Dividend Amount", "Total Purchase in PLN CPI adj"]] = (base[["Total Purchase in PLN", "Value Amount", "Dividend Amount", "Total Purchase in PLN CPI adj"]]
-                                                                                      .add
-                                                                                      (single_ticker_dataframes[i][["Total Purchase in PLN", "Value Amount", "Dividend Amount", "Total Purchase in PLN CPI adj"]]
-                                                                                       , fill_value = 0 ))
+                base[["Total Purchase in PLN"
+                      , "Value Amount"
+                      , "Dividend Amount"
+                      , "Total Purchase in PLN CPI adj"]] = (base[["Total Purchase in PLN"
+                                                                   , "Value Amount"
+                                                                   , "Dividend Amount"
+                                                                   , "Total Purchase in PLN CPI adj"]]
+                                                             .add
+                                                             (single_ticker_dataframes[i][["Total Purchase in PLN"
+                                                                                           , "Value Amount"
+                                                                                           , "Dividend Amount"
+                                                                                           , "Total Purchase in PLN CPI adj"]]
+                                                              , fill_value = 0 ))
 
             base["Average Price in PLN"] = round(base["Total Purchase in PLN"] / base["Value Amount"], 2)
             base["Average Price in PLN CPI adj"] = round(base["Total Purchase in PLN CPI adj"] / base["Value Amount"], 2)
